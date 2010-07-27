c56c11cfbe0acc39735a843d68935c14e44e7df2
Michal Vašíček <michal.vasicek@tapmates.com>
2010-07-27 17:01:25 +0200

    Add of common header file

---
+++ src/Classes/MVIOC/MVIOC.h
@@ -0,0 +1,17 @@
+/*
+ *  MVIOC.h
+ *  IOC
+ *
+ *  Created by Michal Vašíček on 7/27/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#import "MVIOCContainer.h"
+#import "MVIOCProperty.h"
+
+#import "MVIOCFactory.h"
+#import "MVIOCPropertyFactory.h"
+
+#import "MVIOCCache.h"
+#import "MVIOCSingletonCache.h"
