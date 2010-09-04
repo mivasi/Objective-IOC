881e509356b48b0d7cfe11abfc66a3e08f96ccdf
Michal Vašíček <michal.vasicek@tapmates.com>
2010-09-04 12:22:55 +0200

    Defining license

--- src/UnitTests/IOCContainerAddComponentsTests.h
+++ src/UnitTests/IOCContainerAddComponentsTests.h
@@ -1,14 +1,18 @@
-//
-//  IOCContainerTests.h
-//  IOC
-//
-//  Created by Michal Vašíček on 7/17/10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
-//
-//  See Also: http://developer.apple.com/iphone/library/documentation/Xcode/Conceptual/iphone_development/135-Unit_Testing_Applications/unit_testing_applications.html
-
-//  Application unit tests contain unit test code that must be injected into an application to run correctly.
-//  Define USE_APPLICATION_UNIT_TEST to 0 if the unit test code is designed to be linked into an independent test executable.
+// 
+// Copyright 2010 MICHAL VASICEK
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
 
 #define USE_APPLICATION_UNIT_TEST 0
 
